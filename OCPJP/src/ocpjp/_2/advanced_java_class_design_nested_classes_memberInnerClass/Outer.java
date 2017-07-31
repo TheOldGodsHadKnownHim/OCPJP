c62b6a0e0a9a02538b03d434bb08f7df47d97b6a
TheOldGodsHadKnownHim <keithclarges@gmail.com>
2017-07-31 20:07:05 +0100

    testing the working of the static nested class

--- OCPJP/src/ocpjp/_2/advanced_java_class_design_nested_classes_memberInnerClass/Outer.java
+++ OCPJP/src/ocpjp/_2/advanced_java_class_design_nested_classes_memberInnerClass/Outer.java
@@ -1,9 +1,17 @@
 package ocpjp._2.advanced_java_class_design_nested_classes_memberInnerClass;
 
+import static ocpjp._2.advanced_java_class_design_nested_classes_staticNestedClass.Enclosing.Nested;
+
 public class Outer {
 
 	private String greeting = "Hello - this greeting is being accessed from a "
 			+ "method in the Inner class";
+
+	Nested nested = new Nested();
+
+	private String greeting2 = nested.getPrice()
+			+ ": Hello, this variable has been returned from a static inner class and I have imported that "
+			+ "static inner class from another package using a static import";
 
 	// defined at the member level
 	protected class Inner {
@@ -11,6 +19,7 @@ public class Outer {
 		public void go() {
 			// Can access private members of outer class
 			System.out.println(greeting);
+			System.out.println(greeting2);
 		}
 	}
 
--- OCPJP/src/ocpjp/_2/advanced_java_class_design_nested_classes_staticNestedClass/Enclosing.java
+++ OCPJP/src/ocpjp/_2/advanced_java_class_design_nested_classes_staticNestedClass/Enclosing.java
@@ -4,13 +4,23 @@ public class Enclosing {
 	
 	private int total=50;
 	
-		static class Nested{
+		public static class Nested{
 			private int price = 6;
+			
+			public int getPrice(){
+				return this.price;
+			}
 		}
 
 	public static void main(String[] args) {
 		Nested nested = new Nested();
 		System.out.println(nested.price);
+		//below won't compile as variables of the outer class are not accessible to a static inner classes
+		//System.out.println(nested.total);
+		
+		Enclosing.Nested nested2 = new Enclosing.Nested();
+		int x = nested2.price;
+
 				
 	}
 
