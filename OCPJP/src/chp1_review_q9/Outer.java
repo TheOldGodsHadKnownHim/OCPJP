bfb08411af4a09207aaac2c93b40840524763f11
TheOldGodsHadKnownHim <keithclarges@gmail.com>
2017-08-10 21:27:54 +0100

    q9 for review purposes

---
+++ OCPJP/src/chp1_review_q9/Outer.java
@@ -0,0 +1,32 @@
+package chp1_review_q9;
+
+public class Outer {
+	
+	private int x = 24;
+	
+	
+	/**
+	 * This is a method that contains a local inner class. 
+	 * @return x
+	 */
+	public int getX(){
+		String message = "x is " + x;
+		
+		class Inner {
+			 private int x = Outer.this.x;
+			 
+			 public void printX(){
+				 System.out.println(message + x);
+			 }
+		}
+		
+		Inner in = new Inner();
+		in.printX();
+		return x;
+	}
+
+	public static void main(String[] args) {
+			new Outer().getX();
+	}
+
+}
