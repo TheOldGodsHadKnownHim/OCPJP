274286c8ed27a47258acfd338324d77a042588d7
TheOldGodsHadKnownHim <keithclarges@gmail.com>
2018-08-21 20:23:17 +0100

    Adding an example of an overloaded method to the Player class. This is to highlight the fact that you cannot decrease the visibility of a method that you inherit from an interface

--- OCPJP/src/ocpjp/_3/object_oriented_design_principles_example2/Player.java
+++ OCPJP/src/ocpjp/_3/object_oriented_design_principles_example2/Player.java
@@ -10,7 +10,26 @@ package ocpjp._3.object_oriented_design_principles_example2;
 public class Player implements FitnessAssessment {
 	
 	@Override
-	public boolean isFit(int fitnessTestScore) {
+	/*
+	 * You have to have the 'public' access modifier here. This is because you cannot reduce the 
+	 * visibility of the inherited isFit method from the FitnessAssessment interface. What you will
+	 * also notice is that the method declaration in the interface does not contain the 'public'
+	 * access modifier. This does not matter. Under the hood, the compiler provides the 'public' 
+	 * keyword for all interface methods
+	 */
+	 public boolean isFit(int fitnessTestScore) {
+		if (fitnessTestScore > 8) {
+			System.out.println("Player is fit");
+			return true;
+
+		} else {
+			System.out.println("Player is not fit");
+			return false;
+		}
+	}
+	
+	//overloaded isFit method with default access modifier
+	 boolean isFit(long fitnessTestScore) {
 		if (fitnessTestScore > 8) {
 			System.out.println("Player is fit");
 			return true;
